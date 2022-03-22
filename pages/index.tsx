77b3e6c5a63a6e7754850fa8ac4b5843e899757e
Edward Sanchez <edward.sanchez@inetum.com>
2022-03-22 14:27:10 -0500

    [ ADD ] - redux config

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
+        <h1 className="text-6xl font-bold text-sky-violet">
           Welcome to{' '}
           <a className="text-blue-600" href="https://nextjs.org">
             Next.js!
@@ -20,7 +20,7 @@ const Home: NextPage = () => {
 
         <p className="mt-3 text-2xl">
           Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
+          <code className="bg-gray-100 font-mono rounded-md p-3 text-lg">
             pages/index.tsx
           </code>
         </p>
@@ -28,7 +28,7 @@ const Home: NextPage = () => {
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
           <a
             href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            className="hover:text-blue-600 focus:text-blue-600 mt-6 w-96 rounded-xl border p-6 text-left"
           >
             <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
             <p className="mt-4 text-xl">
@@ -38,7 +38,7 @@ const Home: NextPage = () => {
 
           <a
             href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            className="hover:text-blue-600 focus:text-blue-600 mt-6 w-96 rounded-xl border p-6 text-left"
           >
             <h3 className="text-2xl font-bold">Learn &rarr;</h3>
             <p className="mt-4 text-xl">
@@ -48,7 +48,7 @@ const Home: NextPage = () => {
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            className="hover:text-blue-600 focus:text-blue-600 mt-6 w-96 rounded-xl border p-6 text-left"
           >
             <h3 className="text-2xl font-bold">Examples &rarr;</h3>
             <p className="mt-4 text-xl">
@@ -58,7 +58,7 @@ const Home: NextPage = () => {
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            className="hover:text-blue-600 focus:text-blue-600 mt-6 w-96 rounded-xl border p-6 text-left"
           >
             <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
             <p className="mt-4 text-xl">
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'sky-black': '#04142a',
+      'sky-violet': '#4E34E1',
+      'sky-green': '#60d192',
+      'sky-gray': '#6b6b82',
+      'sky-red': '#ef7b89',
+      'sky-blue': '#010057',
+    },
     fontFamily: {
       sans: ['Inter', 'sans-serif'],
     },
